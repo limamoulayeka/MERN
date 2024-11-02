ac8478dfcb3be343f917806d0217b5877465e234
Limamou Laye Ka <limamoulayeka7@gmail.com>
2024-11-02 19:44:16 -0400

    feat: create node project; add express server; install babel

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (_, res) => {
+    res.send("Hello Express !!")
+})
+
+app.listen(port, () => {
+    console.log("serveur démarré avec succès sur le port "+port)
+})
